7a04c2961e94997d6f2b5c4a6c53e8998a5f8cbc
Branata Kurniawan <branata@outloudapp.com>
2026-05-13 09:39:17 +0800

    feat(cli): add CliError type

---
+++ cli/src/lib/errors.ts
@@ -0,0 +1,10 @@
+export class CliError extends Error {
+  constructor(
+    public readonly exitCode: number,
+    public readonly code: string,
+    message: string,
+  ) {
+    super(message);
+    this.name = 'CliError';
+  }
+}
